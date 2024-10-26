28b98bad3dce07bcb1e09d8ca3f8d635d57c4735
Sayari Zeynab <z.sayari@Titec.local>
2024-10-26 15:56:26 +0330

    navbar is finished

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,13 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+    images: {
+        remotePatterns : [
+           {
+            protocol:"https", 
+            hostname:"negativespace.co"
+           }
+        ]
+    } 
+};
 
 export default nextConfig;
--- src/components/CartModal.tsx
+++ src/components/CartModal.tsx
@@ -1,12 +1,86 @@
 'use client'
+import Image from "next/image";
+
 const CartModal = ()=>{
 const cartItems = true;
 
-    return (<div className="absolute p-4 rounded-md shadow-[0_3px_10px_rgb(0,0,0,0.2)] bg-white
+    return (<div className="w-max absolute p-4 rounded-md shadow-[0_3px_10px_rgb(0,0,0,0.2)] bg-white
         top-12 right-0 flex flex-col gap-6 z-20">
             {
-                !cartItems ?  (<div>Cart is empty</div>):(<div></div>)
-            }
+                !cartItems ?  (<div>Cart is empty</div>)
+                    :(
+                        <>
+                        <h2 className="text-xl">Shopping Cart</h2>
+                         {/* LIST */}
+                        <div className="flex flex-col gap-8 ">
+                            {/* ITEM */}
+                        <div className="flex gap-4">
+                <Image 
+                // src="https://negativespace.co/wp-content/uploads/2024/10/negative-space-maple-tree-foliage-1062x708.jpg"
+                    src="/pencils.jpg"
+                 alt="" width={72} height={96} className="object-cover rounded-md"/>
+                    <div className="flex flex-col justify-between w-full">
+                        {/*TOP*/}
+                         <div>
+                            {/*TITLE*/}
+                            <div className="flex items-center justify-between gap-8">
+                                <h3 className="font-semibold">Product Name</h3>
+                                <div className="p-1 bg-gray-50 rounded-sm">49$</div>
+                            </div>
+                            {/*DES*/}
+                            <div className="text-sm text-gray-500">available</div>
+                        </div>
+                        {/*BOTTOM*/}
+                        <div className="flex  justify-between text-sm">
+                            <span className="text-gray-500">Qty. 2</span>
+                            <span className="text-blue-500">Remove</span>
+                        </div>
+                    </div>
+
+                        </div>
+                            {/* ITEM */}
+                            <div className="flex gap-4">
+                                <Image
+                                    // src="https://negativespace.co/wp-content/uploads/2024/10/negative-space-maple-tree-foliage-1062x708.jpg"
+                                    src="/pencils.jpg"
+                                    alt="" width={72} height={96} className="object-cover rounded-md"/>
+                                <div className="flex flex-col justify-between w-full">
+                                    {/*TOP*/}
+                                    <div>
+                                        {/*TITLE*/}
+                                        <div className="flex items-center justify-between gap-8">
+                                            <h3 className="font-semibold">Product Name</h3>
+                                            <div className="p-1 bg-gray-50 rounded-sm">49$</div>
+                                        </div>
+                                        {/*DES*/}
+                                        <div className="text-sm text-gray-500">available</div>
+                                    </div>
+                                    {/*BOTTOM*/}
+                                    <div className="flex  justify-between text-sm">
+                                        <span className="text-gray-500">Qty. 2</span>
+                                        <span className="text-blue-500">Remove</span>
+                                    </div>
+                                </div>
+
+                            </div>
+                    </div>
+                     {/* BOTTOM */}
+                    <div>
+                        <div className="flex items-center justify-between font-semibold ">
+                            <span>SubTotal</span>
+                            <span>49$</span>
+                        </div>
+                        <p className="text-gray-500 text-sm mt-2 mb-4">
+                            Shipping and taxes calculated at Checkout
+                        </p>
+                        {/* BUTTONS */}
+                        <div className="flex justify-between text-sm">
+                            <button className="rounded-md py-3 px-4 ring-1 ring-gray-300">View Cart</button>
+                            <button className="rounded-md py-3 px-4 bg-black text-white" >Checkout</button>
+                        </div>
+                    </div>
+                </>
+                    )}
         </div>); 
 
 
