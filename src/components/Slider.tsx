91e64b4366a98db353417a7c79dc836e0165f990
Sayari Zeynab <z.sayari@Titec.local>
2024-10-28 11:25:30 +0330

    slider is finished

--- src/components/Slider.tsx
+++ src/components/Slider.tsx
@@ -1,5 +1,5 @@
 'use client'
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import Link from "next/link";
 import Image from "next/image";
 
@@ -33,13 +33,21 @@ const slides = [
 const Slider = ()=>{
     const[current , setCurrent] = useState(0);
 
+//  SLIDES MOVING AUTOMATICALLY BY THIS USEeFFECT
+// useEffect(()=>{
+// const interval = setInterval(()=>{
+//  setCurrent((prev)=>(prev === slides.length - 1 ? 0 : prev + 1  ));
+// },3000)
+// return ()=>clearInterval(interval); 
+// },[])
+
     
     return (
     <div className='h-[calc(100vh-80px)]  overflow-hidden'>
         <div className="w-max h-full flex transition-all ease-in-out duration-1000"
         style={{transform: `translateX(-${current * 100 }vw)`}}
         >
-            {slides.map((slide=>
+            {slides.map(((slide)=>
                 <div className={`${slide.bg} w-screen h-full flex flex-col gap-16  xl:flex-row `} key={slide.id}>
                     {/* TEXT CONTAINER */}
                     <div className="h-1/2 xl:w-1/2 xl:h-full flex flex-col items-center justify-center gap-8 2xl:gap-12 text-center">
@@ -51,7 +59,7 @@ const Slider = ()=>{
                     </div>
                     {/* IMAGE CONTAINER */}
                     <div className="h-1/2 xl:w-1/2 xl:h-full relative">
-                        <Image src={slide.img} alt="" fill sizes="100%" className="object-cover"/>
+                        <Image src={slide.img} alt="" fill sizes="100%" className="object-cover" />
                     </div>
                 </div> 
             ))}
@@ -60,7 +68,7 @@ const Slider = ()=>{
         <div className="absolute m-auto left-1/2 bottom-8 flex gap-4 ">
             {slides.map((slide,index)=>(
                 <div className={`w-3  h-3 rounded-full ring-1 ring-gray-600 
-                    cursor-pointer flex items-center justify-center 
+                    cursor-pointer flex items-center justify-center  
                     ${current === index ? "scale-150" :"" }`} 
                     key={slide.id} 
                     onClick={()=>setCurrent(index)}>
